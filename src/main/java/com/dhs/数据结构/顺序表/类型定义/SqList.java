410f02c16b077949e32fc3976c0bd4d49d547261
CoderZhaoWH <2451222478@qq.com>
2018-09-05 20:55:27 +0800

    xiugai

--- src/main/java/com/dhs/数据结构/顺序表/类型定义/SqList.java
+++ src/main/java/com/dhs/数据结构/顺序表/类型定义/SqList.java
@@ -1,6 +1,7 @@
 package com.dhs.数据结构.顺序表.类型定义;
 
 public class SqList {
-    int[] data = new int[100];
-    int length;
+    public static final int MAX_SIZE = 100;
+    public int[] data = new int[100];
+    public int length;
 }
