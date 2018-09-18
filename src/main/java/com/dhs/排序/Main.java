31897f29e5ffb3cfdc504c6bd58ecb11efa94624
CoderZhaoWH <2451222478@qq.com>
2018-09-18 17:55:00 +0800

    xiugai

--- src/main/java/com/dhs/排序/Main.java
+++ src/main/java/com/dhs/排序/Main.java
@@ -1,12 +1,13 @@
 package com.dhs.排序;
 
-import com.dhs.排序.交换排序.冒泡排序.BubbleSort;
-import com.dhs.排序.插入排序.直接插入排序.StraightInsertionSort;
+import com.dhs.排序.交换排序.快速排序.QuickSort;
 
 public class Main {
     public static void main(String[] args) {
         int[] item = {46, 52, 16, 45, 88, 20, 8, 72};
-        StraightInsertionSort.sort(item);
-        BubbleSort.bubbleSort(item);
+        //StraightInsertionSort.sort(item);
+        // BubbleSort.bubbleSort(item);
+        int[] sortArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        QuickSort.quickSort(sortArray, 2, 6);
     }
 }
