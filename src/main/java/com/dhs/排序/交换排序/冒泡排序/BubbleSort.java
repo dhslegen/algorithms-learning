29dd39e7dce3357c20cd263d7a57dea19f88546a
zhaowenhao <dhslegen@163.com>
2019-08-05 15:22:17 +0800

    修改

--- src/main/java/com/dhs/排序/交换排序/冒泡排序/BubbleSort.java
+++ src/main/java/com/dhs/排序/交换排序/冒泡排序/BubbleSort.java
@@ -3,7 +3,8 @@ package com.dhs.排序.交换排序.冒泡排序;
 public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {//-1为了防止溢出
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // -1为了防止溢出
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
 
